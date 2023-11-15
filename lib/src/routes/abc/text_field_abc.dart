aeaec52dd9aa187407fb906393f57465824414fd
angcyo <angcyo@126.com>
2023-11-15 14:20:19 +0800

    + text field abc

--- lib/src/routes/abc/text_field_abc.dart
+++ lib/src/routes/abc/text_field_abc.dart
@@ -19,9 +19,14 @@ class _TextFieldAbcState extends State<TextFieldAbc> with BaseAbcStateMixin {
         'TextField↓',
         textAlign: TextAlign.center,
       ),
-      const TextField(
-        decoration: InputDecoration(
+      TextField(
+        onChanged: (value) => l.v(value),
+        onSubmitted: (value) => toast(Text(value)),
+        enabled: AbcConfig.getAndIncrementClickCount() % 2 == 0,
+        decoration: const InputDecoration(
           labelText: "default",
+          prefixText: 'prefixText',
+          suffixText: 'suffixText',
         ),
       ),
       const TextField(
@@ -29,11 +34,51 @@ class _TextFieldAbcState extends State<TextFieldAbc> with BaseAbcStateMixin {
         obscuringCharacter: "*",
         decoration: InputDecoration(
           labelText: "default-obscure",
+          prefix: Icon(Icons.lock),
+          suffix: Icon(Icons.remove_red_eye),
         ),
       ),
       const TextField(
         decoration: InputDecoration(
           labelText: "default-border",
+          border: OutlineInputBorder(),
+        ),
+      ),
+      const Text(
+        'TextField-lines↓',
+        textAlign: TextAlign.center,
+      ),
+      const TextField(
+        maxLength: 10,
+        maxLines: 2,
+        decoration: InputDecoration(
+          labelText: "default-border",
+          hintMaxLines: 5,
+          border: OutlineInputBorder(),
+        ),
+      ),
+      TextField(
+        maxLength: 50,
+        maxLines: 2,
+        decoration: InputDecoration(
+          suffix: randomTextWidget(length: 5),
+          labelText: "default-border",
+          hintMaxLines: 5,
+          border: const OutlineInputBorder(),
+        ),
+      ),
+      const TextField(
+        maxLength: 100,
+        minLines: 3,
+        maxLines: 10,
+        autofillHints: [
+          AutofillHints.email,
+          AutofillHints.username,
+          AutofillHints.url,
+        ],
+        decoration: InputDecoration(
+          labelText: "default-border",
+          hintMaxLines: 5,
           border: OutlineInputBorder(),
         ),
       ),
