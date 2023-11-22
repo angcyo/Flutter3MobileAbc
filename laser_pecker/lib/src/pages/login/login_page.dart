b7567298de9db3245290a4fbc9a4a9d5b2540774
angcyo <angcyo@126.com>
2023-11-22 18:25:56 +0800

    + login page

--- laser_pecker/lib/src/pages/login/login_page.dart
+++ laser_pecker/lib/src/pages/login/login_page.dart
@@ -13,10 +13,17 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  late final TextEditingController _accountController =
+      TextEditingController(text: "userName");
+  late final TextEditingController _passwordController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     var globalConfig = laserPeckerApp.currentState!.globalConfig;
+    var backgroundColor = "#f9f9f9".toColor();
     return Scaffold(
+      backgroundColor: backgroundColor,
       appBar: globalConfig.appBarBuilder(
         context,
         this,
@@ -32,9 +39,28 @@ class _LoginPageState extends State<LoginPage> {
                   context.pop();
                 }),
         elevation: 0,
+        backgroundColor: backgroundColor,
       ),
       body: [
-        lpSvgWidget(Assets.svg.logo),
+        lpSvgWidget(Assets.svg.logo)
+            .wrapContent(AlignmentDirectional.centerStart)
+            .paddingAll(kXh),
+        SingleInputWidget(
+          controller: _accountController,
+          cursorColor: globalConfig.globalTheme.accentColor,
+          focusBorderColor: globalConfig.globalTheme.accentColor,
+          hintText: "请输入账号",
+        ).paddingOnly(left: kXh, right: kXh, top: kXh),
+        SingleInputWidget(
+          controller: _passwordController,
+          focusBorderColor: globalConfig.globalTheme.accentColor,
+          hintText: "请输入密码",
+        ).paddingAll(kXh),
+        SingleInputWidget(
+          controller: _passwordController,
+          focusBorderColor: globalConfig.globalTheme.accentColor,
+          enabled: false,
+        ).paddingAll(kXh),
       ].rScroll(),
     );
   }
