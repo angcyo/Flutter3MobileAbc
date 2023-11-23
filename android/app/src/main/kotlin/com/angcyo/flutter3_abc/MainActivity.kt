f69747fcced02133fbe24bf66e1157c10ee5e10b
angcyo <angcyo@126.com>
2023-11-23 19:30:26 +0800

    * fix baes

--- android/app/src/main/kotlin/com/angcyo/flutter3_abc/MainActivity.kt
+++ android/app/src/main/kotlin/com/angcyo/flutter3_abc/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.angcyo.flutter3_abc
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //window.decorView.setBackgroundColor(Color.MAGENTA)
+    }
+
 }
