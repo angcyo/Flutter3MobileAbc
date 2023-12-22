d5d5f1de481e45c60c2e17d268c11b7bf675231e
angcyo <angcyo@126.com>
2023-12-22 18:43:19 +0800

    + slide abc

--- lib/src/routes/main_route.dart
+++ lib/src/routes/main_route.dart
@@ -14,6 +14,7 @@ import 'package:flutter3_app/flutter3_app.dart';
 import 'package:laser_pecker/laser_pecker.dart';
 
 import 'abc/expandable_abc.dart';
+import 'abc/slide_abc.dart';
 
 part 'abc/app_bar_abc.dart';
 
@@ -108,6 +109,7 @@ final _flutter3AbcMap = <String, WidgetBuilder>{
   'StickHeaderAbc ': (context) => const StickHeaderAbc(),
   /*'StickAppBarAbc ': (context) => const StickHeaderAbc(),*/
   'ExpandableAbc ': (context) => const ExpandableAbc(),
+  'SlideAbc ': (context) => const SlideAbc(),
 };
 
 class MainAbc extends StatefulWidget {
