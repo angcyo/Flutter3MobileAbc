9ed72b1b8f478553d7b42db9ad6d93a4fcbad24e
angcyo <angcyo@126.com>
2023-12-27 22:40:24 +0800

    * fix abc

--- lib/src/routes/abc/nine_grid_abc.dart
+++ lib/src/routes/abc/nine_grid_abc.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter3_abc/src/routes/main_route.dart';
 import 'package:flutter3_abc/src/utils/abc_debug.dart';
 import 'package:flutter3_app/flutter3_app.dart';
@@ -21,14 +21,27 @@ class _NineGridAbcState extends State<NineGridAbc> with BaseAbcStateMixin {
   List<Widget> buildBodyList(BuildContext context) {
     final width = nextInt(screenWidth.toInt(), min: 40);
     final height = nextInt(screenHeight.toInt(), min: 40);
+    final count0 = nextInt(9, min: 1);
+    final count1 = nextInt(9, min: 1);
+    final count2 = nextInt(9, min: 1);
+    final count3 = nextInt(9, min: 1);
+    final count4 = nextInt(9, min: 1);
+    final count5 = nextInt(9, min: 1);
+    final count6 = nextInt(9, min: 1);
+    final imageList = [
+      DragBean(),
+      DragBean(),
+      DragBean(),
+      DragBean(),
+      DragBean(),
+    ];
     return [
       "单图($width*$height)↓".text(textAlign: TextAlign.center),
       // bigImage参数 单张大图建议使用中等质量图片，因为原图太大加载耗时。
       NineGridView(
-          margin: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: kX),
           padding: const EdgeInsets.all(5),
-          width: width.toDouble(),
-          height: height.toDouble(),
+          alignment: Alignment.centerLeft,
           bigImageWidth: width,
           bigImageHeight: height,
           space: 5,
@@ -38,19 +51,123 @@ class _NineGridAbcState extends State<NineGridAbc> with BaseAbcStateMixin {
           itemBuilder: (BuildContext context, int index) {
             return randomImageWidget(width: width, height: height);
           }),
-      "QQ群样式↓".text(textAlign: TextAlign.center),
+      "normal 样式($count0)↓".text(textAlign: TextAlign.center),
+      // bigImage参数 单张大图建议使用中等质量图片，因为原图太大加载耗时。
+      NineGridView(
+          margin: const EdgeInsets.symmetric(horizontal: kX),
+          padding: const EdgeInsets.all(5),
+          alignment: Alignment.centerLeft,
+          space: 5,
+          color: randomColor(),
+          type: NineGridType.normal,
+          itemCount: count0,
+          itemBuilder: (BuildContext context, int index) {
+            return randomImageWidget();
+          }),
+      "qqGp 样式($count1)↓".text(textAlign: TextAlign.center),
       // 头像需要设置宽、高参数。
       NineGridView(
         width: 120,
         height: 120,
+        alignment: Alignment.center,
         padding: const EdgeInsets.all(5),
         space: 5,
         color: randomColor(),
         type: NineGridType.qqGp,
         //NineGridType.weChatGp, NineGridType.dingTalkGp
-        itemCount: nextInt(30),
+        itemCount: count1,
         itemBuilder: (BuildContext context, int index) {
           return randomImageWidget();
+        },
+      ),
+      "weChat 样式($count2)↓".text(textAlign: TextAlign.center),
+      NineGridView(
+        width: 180,
+        height: 180,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.all(5),
+        space: 2,
+        color: randomColor(),
+        type: NineGridType.weChat,
+        itemCount: count2,
+        itemBuilder: (BuildContext context, int index) {
+          return randomImageWidget();
+        },
+      ),
+      "weChatGp 样式($count3)↓".text(textAlign: TextAlign.center),
+      NineGridView(
+        width: 180,
+        height: 180,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.all(5),
+        space: 2,
+        color: randomColor(),
+        type: NineGridType.weChatGp,
+        itemCount: count3,
+        itemBuilder: (BuildContext context, int index) {
+          return randomImageWidget();
+        },
+      ),
+      "weiBo 样式($count4)↓".text(textAlign: TextAlign.center),
+      NineGridView(
+        width: 100,
+        height: 100,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.all(5),
+        space: 2,
+        color: randomColor(),
+        type: NineGridType.weiBo,
+        itemCount: count4,
+        itemBuilder: (BuildContext context, int index) {
+          return randomImageWidget();
+        },
+      ),
+      "dingTalkGp 样式($count5)↓".text(textAlign: TextAlign.center),
+      NineGridView(
+        width: 100,
+        height: 100,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.all(5),
+        space: 2,
+        color: randomColor(),
+        type: NineGridType.dingTalkGp,
+        itemCount: count5,
+        itemBuilder: (BuildContext context, int index) {
+          return randomImageWidget();
+        },
+      ),
+      "可拖拽($count6)↓".text(textAlign: TextAlign.center),
+      // 建议使用略微缩图，因为原图太大可能会引起重复加载导致闪动.
+      DragSortView(
+        imageList,
+        space: 5,
+        margin: EdgeInsets.all(20),
+        padding: EdgeInsets.all(0),
+        itemBuilder: (BuildContext context, int index) {
+          return randomImageWidget();
+        },
+        initBuilder: (BuildContext context) {
+          return InkWell(
+            onTap: () {
+              //_loadAssets(context);
+            },
+            child: Container(
+              color: const Color(0XFFCCCCCC),
+              child: const Center(
+                child: Icon(
+                  Icons.add,
+                ),
+              ),
+            ),
+          );
+        },
+        onDragListener: (event, itemWidth) {
+          /// 判断拖动到指定位置删除
+          /// return true;
+          if ((event.globalY ?? 0) > 600) {
+            return true;
+          }
+          return false;
         },
       ),
     ];
