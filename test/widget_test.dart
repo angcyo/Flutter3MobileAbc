c9ba48e2916f83b0654a04ee4863d3016a27fe47
angcyo <angcyo@126.com>
2024-01-03 09:54:10 +0800

    + test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,12 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter3_app/flutter3_app.dart';
 
-import 'package:flutter3_abc/main.dart';
-
+@pragma("vm:entry-point")
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testTime();
+  /*testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
@@ -26,5 +25,23 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
-  });
+  });*/
+}
+
+/// 测试循环100w次的耗时
+@pragma("vm:entry-point")
+void testTime() {
+  assert(() {
+    var c = 0;
+    while (c++ < 10) {
+      lTime.tick();
+      int count = 10000000 * 10 * (c + 1);
+      var sum = 0;
+      for (int i = 0; i < count; i++) {
+        sum += i;
+      }
+      l.i('循环$count次:$sum, 耗时:${lTime.time()}');
+    }
+    return true;
+  }());
 }
