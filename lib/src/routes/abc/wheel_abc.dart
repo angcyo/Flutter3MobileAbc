7416575d732ce3070afb99ce172fb5947bba17ef
angcyo <angcyo@126.com>
2024-05-08 09:46:16 +0800

    * wheel abc

--- lib/src/routes/abc/wheel_abc.dart
+++ lib/src/routes/abc/wheel_abc.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter3_abc/src/routes/main_route.dart';
 import 'package:flutter3_app/flutter3_app.dart';
 
@@ -17,53 +17,70 @@ class WheelAbc extends StatefulWidget {
 class _WheelAbcState extends State<WheelAbc> with BaseAbcStateMixin {
   WidgetList buildItems() {
     return [
-      Text('1'),
-      Text('2'),
-      Text('3'),
-      Text('4'),
-      Text('5'),
-      Text('6'),
-      Text('7'),
-      Text('8'),
-      Text('9'),
-      Text('10'),
+      for (var i = 0; i < 10; i++)
+        Text('$i', textAlign: TextAlign.center).align(Alignment.center),
     ];
   }
+
+  final _itemExtent = 40.0;
+  int _selectedFruit = 3;
 
   @override
   List<Widget> buildBodyList(BuildContext context) {
     return [
       "WheelPicker↓".text(textAlign: TextAlign.center),
       Wheel(
+        itemExtent: _itemExtent,
         children: buildItems(),
       ).container(color: randomColor()),
       Wheel(
         looping: true,
         size: 300,
+        itemExtent: _itemExtent,
         children: buildItems(),
       ).container(color: randomColor()),
       "ListWheelScrollView↓".text(textAlign: TextAlign.center),
       ListWheelScrollView(
-        itemExtent: 30,
+        itemExtent: _itemExtent,
         overAndUnderCenterOpacity: 0.5,
         diameterRatio: 0.8,
+        physics: const FixedExtentScrollPhysics(),
+        controller: FixedExtentScrollController(),
         children: buildItems(),
       ).size(height: 200).container(color: randomColor()),
       "ListWheelScrollViewX↓".text(textAlign: TextAlign.center),
       ListWheelScrollViewX(
-        itemExtent: 30,
+        itemExtent: _itemExtent,
         overAndUnderCenterOpacity: 0.5,
         diameterRatio: 0.8,
         scrollDirection: Axis.horizontal,
         children: buildItems(),
       ).size(height: 200).container(color: randomColor()),
       ListWheelScrollViewX(
-        itemExtent: 30,
+        itemExtent: _itemExtent,
         overAndUnderCenterOpacity: 0.5,
         diameterRatio: 0.8,
         scrollDirection: Axis.vertical,
         children: buildItems(),
       ).size(height: 200).container(color: randomColor()),
+      "CupertinoPicker↓".text(textAlign: TextAlign.center),
+      CupertinoPicker(
+        magnification: 1.22,
+        squeeze: 1.2,
+        useMagnifier: true,
+        itemExtent: _itemExtent,
+        // This sets the initial item.
+        scrollController: FixedExtentScrollController(
+          initialItem: _selectedFruit,
+        ),
+        // This is called when selected item is changed.
+        onSelectedItemChanged: (int selectedItem) {
+          /*setState(() {
+            _selectedFruit = selectedItem;
+          });*/
+        },
+        children: buildItems(),
+      ).size(height: 200),
     ];
   }
 }
