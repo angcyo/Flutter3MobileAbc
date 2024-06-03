b0813cc6870c299f4f0a89d63be79a36967588a4
angcyo <angcyo@126.com>
2024-06-03 17:02:20 +0800

    * fix test

--- test/file_rename_test.dart
+++ test/file_rename_test.dart
@@ -9,6 +9,18 @@ import 'package:flutter_test/flutter_test.dart';
 ///
 
 void main() {
+  test('test', () {
+    final List<List<int>> list = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ];
+    consoleLog(list.runtimeType);
+    consoleLog(list);
+    consoleLog(list[1][1]);
+    return true;
+  });
+
   test('get float', () {
     const string = "-1ri-1.0gh+9tshift1.00mm.dat";
     final floatResult = string.getFloatList();
@@ -18,7 +30,8 @@ void main() {
   });
 
   test('test rename', () {
-    const path = r'E:\hingin\振镜校准\160x160角点补偿';
+    //const path = r'E:\hingin\振镜校准\160x160角点补偿';
+    const path = r'E:\hingin\振镜校准\201x201角点补偿';
     final files = path.listFilesSync(recursive: true) ?? [];
 
     for (final file in files) {
@@ -31,10 +44,41 @@ void main() {
       final fileName = file.fileName();
       String newName = fileName;
 
-      newName = newName.replaceAll("topshift", "y-");
+      final xList = [12, 25, 37, 50, 75, 100];
+      final yList = [10, 20, 30, 40, 61, 81];
+
+      newName = newName.replaceAll("topshift0.25mm", "y-${yList[0]}");
+      newName = newName.replaceAll("topshift0.5mm", "y-${yList[1]}");
+      newName = newName.replaceAll("topshift0.75mm", "y-${yList[2]}");
+      newName = newName.replaceAll("topshift1.00mm", "y-${yList[3]}");
+      newName = newName.replaceAll("topshift1.5mm", "y-${yList[4]}");
+      newName = newName.replaceAll("topshift2.0mm", "y-${yList[5]}");
+
+      newName = newName.replaceAll("downshift0.25mm", "y+${yList[0]}");
+      newName = newName.replaceAll("downshift0.5mm", "y+${yList[1]}");
+      newName = newName.replaceAll("downshift0.75mm", "y+${yList[2]}");
+      newName = newName.replaceAll("downshift1.00mm", "y+${yList[3]}");
+      newName = newName.replaceAll("downshift1.5mm", "y+${yList[4]}");
+      newName = newName.replaceAll("downshift2.0mm", "y+${yList[5]}");
+
+      newName = newName.replaceAll("leftshift0.25mm", "x-${xList[0]}");
+      newName = newName.replaceAll("leftshift0.5mm", "x-${xList[1]}");
+      newName = newName.replaceAll("leftshift0.75mm", "x-${xList[2]}");
+      newName = newName.replaceAll("leftshift1.00mm", "x-${xList[3]}");
+      newName = newName.replaceAll("leftshift1.5mm", "x-${xList[4]}");
+      newName = newName.replaceAll("leftshift2.0mm", "x-${xList[5]}");
+
+      newName = newName.replaceAll("rightshift0.25mm", "x+${xList[0]}");
+      newName = newName.replaceAll("rightshift0.5mm", "x+${xList[1]}");
+      newName = newName.replaceAll("rightshift0.75mm", "x+${xList[2]}");
+      newName = newName.replaceAll("rightshift1.00mm", "x+${xList[3]}");
+      newName = newName.replaceAll("rightshift1.5mm", "x+${xList[4]}");
+      newName = newName.replaceAll("rightshift2.0mm", "x+${xList[5]}");
+
+      /*newName = newName.replaceAll("topshift", "y-");
       newName = newName.replaceAll("downshift", "y+");
       newName = newName.replaceAll("leftshift", "x-");
-      newName = newName.replaceAll("rightshift", "x+");
+      newName = newName.replaceAll("rightshift", "x+");*/
 
       /*newName = newName.replaceAll(r"0.25mm", "25");
       newName = newName.replaceAll(r"0.5mm", "50");
@@ -43,34 +87,33 @@ void main() {
       newName = newName.replaceAll(r"1.5mm", "150");
       newName = newName.replaceAll(r"2.0mm", "200");*/
 
-      newName = newName.replaceAll(r"x-25", "x-12");
-      newName = newName.replaceAll(r"x-50", "x-24");
-      newName = newName.replaceAll(r"x-75", "x-37");
-      newName = newName.replaceAll(r"x-100", "x-49");
-      newName = newName.replaceAll(r"x-150", "x-74");
-      newName = newName.replaceAll(r"x-200", "x-99");
+      /*newName = newName.replaceAll(r"x-25", "x-${xList[0]}");
+      newName = newName.replaceAll(r"x-50", "x-${xList[1]}");
+      newName = newName.replaceAll(r"x-75", "x-${xList[2]}");
+      newName = newName.replaceAll(r"x-100", "x-${xList[3]}");
+      newName = newName.replaceAll(r"x-150", "x-${xList[4]}");
+      newName = newName.replaceAll(r"x-200", "x-${xList[5]}");
 
-      newName = newName.replaceAll(r"x+25", "x+12");
-      newName = newName.replaceAll(r"x+50", "x+24");
-      newName = newName.replaceAll(r"x+75", "x+37");
-      newName = newName.replaceAll(r"x+100", "x+49");
-      newName = newName.replaceAll(r"x+150", "x+74");
-      newName = newName.replaceAll(r"x+200", "x+99");
+      newName = newName.replaceAll(r"x+25", "x+${xList[0]}");
+      newName = newName.replaceAll(r"x+50", "x+${xList[1]}");
+      newName = newName.replaceAll(r"x+75", "x+${xList[2]}");
+      newName = newName.replaceAll(r"x+100", "x+${xList[3]}");
+      newName = newName.replaceAll(r"x+150", "x+${xList[4]}");
+      newName = newName.replaceAll(r"x+200", "x+${xList[5]}");
 
-      newName = newName.replaceAll(r"y-25", "y-10");
-      newName = newName.replaceAll(r"y-50", "y-20");
-      newName = newName.replaceAll(r"y-75", "y-30");
-      newName = newName.replaceAll(r"y-100", "y-40");
-      newName = newName.replaceAll(r"y-150", "y-60");
-      newName = newName.replaceAll(r"y-200", "y-80");
+      newName = newName.replaceAll(r"y-25", "y-${yList[0]}");
+      newName = newName.replaceAll(r"y-50", "y-${yList[1]}");
+      newName = newName.replaceAll(r"y-75", "y-${yList[2]}");
+      newName = newName.replaceAll(r"y-100", "y-${yList[3]}");
+      newName = newName.replaceAll(r"y-150", "y-${yList[4]}");
+      newName = newName.replaceAll(r"y-200", "y-${yList[5]}");
 
-      newName = newName.replaceAll(r"y+25", "y+10");
-      newName = newName.replaceAll(r"y+50", "y+20");
-      newName = newName.replaceAll(r"y+75", "y+30");
-      newName = newName.replaceAll(r"y+100", "y+40");
-      newName = newName.replaceAll(r"y+150", "y+60");
-      newName = newName.replaceAll(r"y+200", "y+80");
-
+      newName = newName.replaceAll(r"y+25", "y+${yList[0]}");
+      newName = newName.replaceAll(r"y+50", "y+${yList[1]}");
+      newName = newName.replaceAll(r"y+75", "y+${yList[2]}");
+      newName = newName.replaceAll(r"y+100", "y+${yList[3]}");
+      newName = newName.replaceAll(r"y+150", "y+${yList[4]}");
+      newName = newName.replaceAll(r"y+200", "y+${yList[5]}");*/
 
       file.renameTo(newName);
     }
