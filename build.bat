7ed44ed2b91bb1fa9f6b1c248c1f9c54b702c6c7
angcyo <angcyo@126.com>
2024-06-17 19:03:28 +0800

    * fix build bat

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 
 :: build/app/outputs/apk/release/app-release.apk
 :: --build-name  --build-number
-flutter build apk --release
+flutter build apk --release --build-name "1.0.0" --build-number "1"
 
-::flutter build ios --release
+::flutter build ios --release --build-name "1.0.0" --build-number "1"
