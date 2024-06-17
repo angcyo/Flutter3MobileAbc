8c8ae18d818bab532b8c9564ce07b7399de753c8
angcyo <angcyo@126.com>
2024-06-17 18:33:53 +0800

    * fix base

---
+++ build.bat
@@ -0,0 +1,7 @@
+:: https://docs.flutter.dev/deployment/android
+
+:: build/app/outputs/apk/release/app-release.apk
+:: --build-name  --build-number
+flutter build apk --release
+
+::flutter build ios --release
