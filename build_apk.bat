24963469585642ea91d6d9cfb3185b87bec6a779
angcyo <angcyo@126.com>
2024-06-20 16:10:21 +0800

    * fix base

--- build_apk.bat
+++ build_apk.bat
@@ -1,7 +1,11 @@
+dart run Flutter3Core/build.dart | echo "Build done"
+::dart run build_runner build
+
 :: https://docs.flutter.dev/deployment/android
 
 :: build/app/outputs/apk/release/app-release.apk
 :: --build-name  --build-number
-flutter build apk --release --build-name "1.0.0" --build-number "1"
+::flutter build apk --release --build-name "1.0.0" --build-number "1"
 
 ::flutter build ios --release --build-name "1.0.0" --build-number "1"
+::echo "Build done"
