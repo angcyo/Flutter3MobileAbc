599655bf66d6b8a21578d8c957d3c22cec42ee6b
angcyo <angcyo@126.com>
2024-07-23 22:35:29 +0800

    * fix base

--- lib/src/routes/abc/shelf_abc.dart
+++ lib/src/routes/abc/shelf_abc.dart
@@ -35,15 +35,31 @@ class _ShelfAbcState extends State<ShelfAbc> with BaseAbcStateMixin {
       updateState();
     });
 
+  String? _uploadFilePath;
+
+  //--
+
   late final shelf.DebugLogWebSocketServer _debugShelf =
       shelf.DebugLogWebSocketServer();
 
-  String? _uploadFilePath;
+  //--
+
+  /// udp 广播接收
+  shelf.UDP? _receiveUdp;
+
+  /// udp 广播端口
+  final int _udpBroadcastPort = 9299;
+
+  /// udp 广播接收数据集合
+  final List<String> _receiveUdpList = [];
+
+  //--
 
   @override
   void dispose() {
     _shelf.stop();
     _debugShelf.stop();
+    _receiveUdp?.close();
     super.dispose();
   }
 
@@ -58,6 +74,7 @@ class _ShelfAbcState extends State<ShelfAbc> with BaseAbcStateMixin {
         GradientButton.normal(() {
           _shelf.stop();
         }, child: "停止服务".text()),
+        //
         GradientButton.normal(() async {
           await _debugShelf.start();
           updateState();
@@ -65,6 +82,30 @@ class _ShelfAbcState extends State<ShelfAbc> with BaseAbcStateMixin {
         GradientButton.normal(() {
           _debugShelf.stop();
         }, child: "停止Debug服务".text()),
+        //
+        GradientButton.normal(() async {
+          shelf.sendUdpBroadcast(
+            _udpBroadcastPort,
+            text: "广播:Hello udp:${nowTimeString()}",
+          );
+        }, child: "发送UDP广播".text()),
+        GradientButton.normal(() async {
+          _receiveUdp ??= await shelf.receiveUdpBroadcast(
+            _udpBroadcastPort,
+            onDatagramAction: (datagram) {
+              datagram?.data.utf8Str.let((it) {
+                if (!_receiveUdpList.contains(it)) {
+                  _receiveUdpList.add(it);
+                  updateState();
+                }
+              });
+            },
+          );
+        }, child: "接收UDP广播".text()),
+        GradientButton.normal(() async {
+          _receiveUdp?.close();
+          _receiveUdp = null;
+        }, child: "停止接收UDP广播".text()),
       ].flowLayout(childGap: kX, padding: const EdgeInsets.all(kX))!,
       if (!isNil(_shelf.address))
         [
@@ -94,6 +135,11 @@ class _ShelfAbcState extends State<ShelfAbc> with BaseAbcStateMixin {
             //_uploadFilePath?.openFile();
           }),
           CanvasFilePreviewWidget(_uploadFilePath!.file()),
+        ].column(crossAxisAlignment: CrossAxisAlignment.start)!.paddingAll(kX),
+      if (_receiveUdpList.isNotEmpty)
+        [
+          "收到UDP广播:".text(),
+          ..._receiveUdpList.map((e) => e.text(textColor: Colors.blue)),
         ].column(crossAxisAlignment: CrossAxisAlignment.start)!.paddingAll(kX),
     ];
   }
