0288c00748dbefb393b4c2fe86f3cc6cb7c3d9cd
angcyo <angcyo@126.com>
2024-12-25 16:10:43 +0800

    init shell

--- flutter3_fetch.bat
+++ flutter3_fetch.bat
@@ -3,32 +3,28 @@ rem 设置当前控制台为UTF-8编码
 chcp 65001 >> nul
 
 set "coreFolder=Flutter3Core"
-
 IF EXIST "%CD%\%coreFolder%\" (
     cd %coreFolder%
     echo 准备拉取仓库：%coreFolder%
     git fetch
     git rebase origin/main
+    cd ..
 ) else (
     echo 准备克隆仓库：%coreFolder%
     git clone git@gitee.com:angcyo/Flutter3Core.git 
 )
 
-cd ..
-
 set "extendFolder=Flutter3Extend"
-
 IF EXIST "%CD%\%extendFolder%\" (
     cd %extendFolder%
     echo 准备拉取仓库：%extendFolder%
     git fetch
     git rebase origin/main
+    cd ..
 ) else (
     echo 准备克隆仓库：%extendFolder%
     git clone git@gitee.com:angcyo/Flutter3Extend.git
 )
-
-cd ..
 
 echo "结束"
 pause
--- flutter3_fetch.command
+++ flutter3_fetch.command
@@ -4,31 +4,27 @@
 set +x
 
 coreFolder="Flutter3Core"
-
 if [ -d "$PWD/$coreFolder" ]; then
     cd $coreFolder
     echo "准备拉取仓库：$coreFolder"
     git fetch
     git rebase origin/main
+    cd ..
 else
     echo "准备克隆仓库：$coreFolder"
     git clone git@gitee.com:angcyo/Flutter3Core.git
 fi
 
-cd ..
-
 extendFolder="Flutter3Extend"
-
 if [ -d "$PWD/$extendFolder" ]; then
     cd $extendFolder
     echo "准备拉取仓库：$extendFolder"
     git fetch
     git rebase origin/main
+    cd ..
 else
     echo "准备克隆仓库：$extendFolder"
     git clone git@gitee.com:angcyo/Flutter3Extend.git
 fi
-
-cd ..
 
 read -n 1 -s -r -p "!结束! Press any key to continue..."
